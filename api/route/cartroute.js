4fd4d94af2ef63bbc366a0f0df2f4dd8e708f466
DeepakParihar221 <deepakparihar223@gmail.com>
2024-08-29 23:51:47 +0530

    New APIs added

--- api/route/cartroute.js
+++ api/route/cartroute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { test } from "../controllers/cartadd.js";
-import {addToCart, checkout, generateDiscountCode} from "../controllers/cartadd.js";
+import {addToCart, checkout, generateDiscountCode,getDetails} from "../controllers/cartadd.js";
 
 const router = express.Router();
 
@@ -9,6 +9,6 @@ router.get('/test', test);
 router.post('/addtocart', addToCart);
 router.post('/checkout', checkout);
 router.post('/generateDiscountCode', generateDiscountCode);
-
+router.post('/getDetails',getDetails);
 
 export default router ;
